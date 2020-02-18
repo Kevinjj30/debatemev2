820099ee92790053f1e13817c47b2cb4ed8c6088
kevinjj30 <kevinjenkins56@yahoo.com>
2020-02-18 16:57:01 -0600

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import UserStore from './stores/userStore';
+import './App.css';
+
+class App extends React.Component {
+
+  render() {
+    return(
+    <div className="app">
+    testing
+    </div>
+  );
+}
+
+}
+export default App;
